817b3e05bc74b6cc87b0a36d5f978eda711954b5
DevEmman <eolasunkanmi@gmail.com>
2024-12-09 21:07:39 +0000

    added condition type of typescript

---
+++ conditionals.ts
@@ -0,0 +1,27 @@
+// IMPORTING LIBRARIES
+import fetch from "node-fetch";
+interface PokemonResults {
+    count: number;
+    next?: string;
+    previous?: string;
+    results: {
+        name: string;
+        url: string;
+    }[];
+}
+
+type FetchPokemonResult<T> = T extends undefined ? Promise<PokemonResults[]>: void;
+
+function fetchPokemon<T extends undefined | ((data: PokemonResults[]) => void)> (
+    url: string,
+    cb?: T
+) : FetchPokemonResult<T> {
+   if (cb) {
+    fetch(url)
+      .then(resp => resp.json())  
+      .then(cb);
+      return undefined as FetchPokemonResult<T>;
+   } else {
+    return fetch(url).then((resp) => resp.json()) as FetchPokemonResult<T>;  
+   }
+}
