368cbec6b110e21d75b9a47ef3fd81458467e5a2
DevEmman <eolasunkanmi@gmail.com>
2024-12-11 23:48:45 +0000

    added conditional type of typescript and contional-redone

--- conditionals-redone.ts
+++ conditionals-redone.ts
@@ -11,19 +11,18 @@ interface PokemonResults {
     }[];
 }
 
-type FetchPokemonResult<T> = T extends undefined ? Promise<PokemonResults>: void;
 
-function fetchPokemon<T extends undefined | ((data: PokemonResults) => void)> (
-    url: string,
-    cb?: T
-): FetchPokemonResult<T> {
+function fetchPokemon(url: string, cb: (data: PokemonResults) => void): void;
+function fetchPokemon (url: string): Promise<PokemonResults>;
+function fetchPokemon (url: string, cb?: (data: PokemonResults) => void): unknown
+  {
    if (cb) {
     fetch(url)
       .then((resp) => resp.json())  
       .then(cb);
-      return undefined as FetchPokemonResult<T>;
+      return undefined;
    } else {
-    return fetch(url).then((resp) => resp.json()) as FetchPokemonResult<T>;  
+    return fetch(url).then((resp) => resp.json());  
    }
 }
 
@@ -35,4 +34,7 @@ fetchPokemon("https://pokeapi.co/api/v2/pokemon?limit=10", (data) => {
 //     const data = await fetchPokemon("https://pokeapi.co/api/v2/pokemon?limit=10")
 //     data.results.forEach((pokemon) => console.log(pokemon.name));
 //   })();
+
+
+
 
